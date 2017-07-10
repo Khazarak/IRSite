125a0d5f041f6d54b73afa7972384783331ebc03
Chris <Khazarak@gmail.com>
2017-07-10 03:42:20 -0400

    Add post model, link it in admin, and display post data to home site.

--- insanityrising/posts/models.py
+++ insanityrising/posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    author = models.CharField(max_length=100)
+    title = models.CharField(max_length=250)
+    pub_date = models.DateField()
+    content = models.TextField()
+    images = models.ImageField()
