3a310dbee8d95d49b59b0e86c04c69d6a22c0063
Chris <Khazarak@gmail.com>
2017-08-01 23:21:58 -0400

    Refactor the templates to extend a base.html

--- insanityrising/apply/models.py
+++ insanityrising/apply/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.mail import send_mail
+from django.utils import timezone
 
 APPLICATION_STATUS = (
     ('a', 'Accepted'),
@@ -12,8 +14,11 @@ class Apply(models.Model):
     applicant_age = models.IntegerField()
     applicant_favmmo = models.CharField(max_length=100)
     about_you = models.TextField()
+    apply_date = models.DateTimeField(default=timezone.now())
     status = models.CharField(max_length=1, choices=APPLICATION_STATUS, default='u')
 
     def __str__(self):
         return self.applicant_name
 
+    def send_conf_email(self, subject, message, fromEmail, toEmail):
+        send_mail(subject,message,fromEmail,toEmail, fail_silently=False)
--- insanityrising/apply/views.py
+++ insanityrising/apply/views.py
@@ -12,6 +12,9 @@ def apply(request):
         apply_inst.applicant_favmmo = request.POST['favoritemmo']
         apply_inst.about_you = request.POST['about_you']
         apply_inst.save()
+        subject = "Insanity Rising application confirmation"
+        message = "This email is a Confirmation of your application being submitted. \nThanks, \nThe Insanity Rising Team."
+        apply_inst.send_conf_email(subject, message, "support@insanityrising.com", request.POST['email'])
         return HttpResponseRedirect("Application submitted")
     else:
         return render(request, 'apply/apply.html')
